8ab7b5c22761cf31eb430ac62f19c2c990487118
[john gunnarsson] <[johnmessoa@gmail.com]>
2024-08-17 17:25:30 +0200

    made chanches in dropdown, added icon isntead of doing it in css

--- src/component/dropdown/Dropdown.jsx
+++ src/component/dropdown/Dropdown.jsx
@@ -3,7 +3,7 @@ import "../dropdown/Dropdown.css"
 import { AuthContext } from "../context/AuthContext";
 import { authenticate, authenticateRole} from "../../helper";
 import { Link } from "react-router-dom";
-
+import { BiMenu } from "react-icons/bi";
 
 const Dropdown = () => {
     const {logout} = useContext(AuthContext);
@@ -38,22 +38,22 @@ const Dropdown = () => {
         <nav id="navbar" className="navigation" role="navigation">
             <input id="toggle-dropdown" type="checkbox" onClick={handleClick} />
             <label className="dropdown" htmlFor="toggle-dropdown">
-                {/* <div className="top"></div>
-                <div className="middle"></div>
-                <div className="bottom"></div> */}
+                <div >
+                    <BiMenu className="dropdown-react-menu-icon"/>
+                </div>
             </label>
         </nav>
     </div>
 
     <div className={isActive ? "dropdown-active" : "dropdown-not-active"}>
-          {loggedInUser ? <><p className="dd-misc">{loggedInUser.username}</p><p className="dd-misc">____________</p></> : null}
+          {loggedInUser ? <><p className="dd-misc">{loggedInUser.username}</p><p className="dd-misc"></p></> : null}
           {authRole ? <Link className="dd-link" to="/admin">Admin</Link> : null}
           {auth ? <Link className="dd-link" to="/profile">Profile</Link> : null}
           <Link className="dd-link" to="/about">About</Link>
           <Link className="dd-link" to="/contact">Contact</Link>
-          {auth ? null : <><p className="dd-misc">____________</p><Link className="dd-link" to="/signup">Register</Link></>}
+          {auth ? null : <><p className="dd-misc"></p><Link className="dd-link" to="/signup">Register</Link></>}
           {auth ? null : <Link className="dd-link" to="/login">Sign in</Link>}
-          {auth ? <><p className="dd-misc">____________</p><Link className="dd-link" to="/login" onClick={handleLogout}>Sign out</Link></> : null}
+          {auth ? <><p className="dd-misc"></p><Link className="dd-link" to="/login" onClick={handleLogout}>Sign out</Link></> : null}
       </div>
     </>
   )
